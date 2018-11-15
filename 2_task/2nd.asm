25d120d99b2c33e6be031bc3e68f9dec88fc7999
Skullcr1 <44177622+Skullcr1@users.noreply.github.com>
2018-11-15 23:44:18 +0200

    Update 2_task/2nd.asm
    working with letter cases

--- 2_task/2nd.asm
+++ 2_task/2nd.asm
@@ -6,8 +6,18 @@
   text_length DB ?
   read_buffer db 10 dup (?)
   data_descr dw ?
-  buffer_data dw 0
+  buffer_number dw 0
   error_message db "error$"
+
+  ;*************************
+  symbol_number db ?
+  word_number db?
+  lcase_letter db ?
+  dcase_letter db ?    
+
+  ;we will put our result in variables
+
+
 .code
 
 
@@ -17,38 +27,62 @@ mov ds, ax
 
 Open_file:
     mov ah, 3Dh
-    mov al, 00h
-    mov dx, txt_file
+    mov al, 0
+    lea dx, txt_file
     int 21h
     jc reading_error
     mov data_descr, ax
     jmp count_numbers
 
 count_numbers:
-mov ah, 3Fh
-mov bx, data_descr
-mov cx, 10
-lea dx, read_buffer
-int 21h
-jc error_message
+    mov ah, 3Fh
+    mov bx, data_descr
+    mov cx, 10
+    lea dx, read_buffer
+    int 21h
+    jc reading_error
+    mov buffer_number, ax
+    
+    call Count_symbols
 
 
 
 
 
 reading_error:
-mov ah, 09h
-mov dx, error_message
-int 21h
-jmp exit
+    mov ah, 09h
+    mov dx, error_message
+    int 21h
+    jmp exit
 
 
 exit:
-
-
-
     mov ah, 4ch
     mov al, 0
     int 21h
-
 end start
+
+PROC Count_symbols
+    push ax
+    push bx
+    push cx
+    push dx
+
+        lea bx, read_buffer
+        mov cx, buffer_number
+
+        cmp [bx], 41h
+        jb not_letter
+        cmp [bx], 5Bh
+        ja big_letter
+        cmp [bx], 61h
+        jb not_letter
+        cmp[bx], 7Bh
+        jb small_letter
+        
+        
+
+       
+
+       
+    
