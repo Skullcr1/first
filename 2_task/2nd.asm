7f652bb73d071472e986f239ae9898e9e699c009
Skullcr1 <44177622+Skullcr1@users.noreply.github.com>
2018-11-27 22:11:42 +0200

    formatted

--- 2_task/2nd.asm
+++ 2_task/2nd.asm
@@ -18,8 +18,8 @@
   ;*************************
   symbol_number dw 0h
   word_number dw 0h
-  lcase_letter dw 0h
-  dcase_letter dw 0h    
+  lower_case dw 0h
+  upper_case dw 0h    
 
   ;we will put our result in variables
 
@@ -36,7 +36,7 @@ start:
     je help
     cmp es:[82h], '?/'
     jne check_parametrs
-    cmp byte ptr es:[84h], 13
+    cmp byte ptr es:[84h], 0dh
     je name1
     jmp check_parametrs
 name1:
@@ -51,7 +51,7 @@ help:
 	jmp exit
 
 check_parametrs:
-	cmp byte ptr es:[bx], 13 
+	cmp byte ptr es:[bx], 0dh 
 	je Open_file
     mov dl, byte ptr es:[bx]
 	mov [si], dl
@@ -62,7 +62,7 @@ check_parametrs:
 
 Open_file:
     mov ah, 3Dh
-    mov al, 0
+    mov al, 0h
     lea dx, Open_file1
     int 21h
     jc reading_error
@@ -82,50 +82,38 @@ reading_from_buffer:
     lea dx, read_buffer
     int 21h
     
-    ;push bx
     jc reading_error
     mov buffer_number, ax 
-    cmp buffer_number, 0
-    ja SCREWING_THROUGH_BUFFER
-    ; pop bx
+    cmp buffer_number, 0h
+    ja ITERATE_BUFFER
     jmp close_file
 
-SCREWING_THROUGH_BUFFER:
+ITERATE_BUFFER:
         xor bx, bx
-        ; mov bx, dx
-       mov bx, ax
+        mov bx, ax
         lea si, read_buffer   
       
-        mov cx, ax
+        mov cx, ax      ;amount of characters 
+        pop bx          ;last value from previous buffer
+        mov ah, bl      ;saving last value in ah
 
-        pop bx
-        mov ah, bl
-
-        lol:
-        
-        
-        
-
-    ;rename
-        
+        check_if_iterate_finished:  
             cmp cx, 0h
-            jne continue_checking
-            
-            push bx
+            jne check_char_ranges
+            push bx     ;save last value to stack
             jmp reading_from_buffer 
         
 
-continue_checking:
+check_char_ranges:
             mov bx, [si]
             cmp bl, 0h
             je reading_from_buffer
-        checking:  
             cmp bl, 0Dh
-            je subber
+            je increase_counter
             cmp bl, 0Ah
-            je subber
+            je increase_counter
             cmp bl, 20h
-            je subber
+            je increase_counter
             cmp bl, 61h
             jae small_letter
             cmp bl, 41h
@@ -136,81 +124,38 @@ continue_checking:
             cmp ah, 20h
             ja word_count
 
-        ; space1:
-        ;     cmp ax, cx
-        ;     je space2
-        ;     cmp bh, 20h
-        ;     ja word_count
-        ;     jmp subber
-        ; spacex:      
-        ;     cmp cx, 0Ah  
-        ;     je subber
-        ;     jmp space2
-        ; space2:    
-        ;     cmp bh, 20h
-        ;     je subber
-
-
-         subber:
-            ;mov bl, bh
+         increase_counter: 
             inc si
             dec cx
-            jmp lol
-
-        ;  st_space:
-        ;     mov bl, bh
-        ;     inc si
-        ;     dec cx
-        ;     jmp lol
+            jmp check_if_iterate_finished
 
         word_count:
-            
-            inc word_number     
-           ; mov bl,bh    
+            inc word_number        
             inc si
             dec cx
-            jmp lol
-        
-        ; not_letter:  
-        ;     dec cx
-        ;     inc symbol_number
-        ;     ;mov bl,bh
-        ;     inc si
-        ;     jmp lol
-
-        ; big_letter: ;/* check */
-        ;     cmp bl, 5Ah
-        ;     ja not_letter
-        ;     inc dcase_letter
-        ;     dec cx
-        ;     inc symbol_number
-        ;     mov bl,bh
-        ;     inc si
-        ;     jmp lol
+            jmp check_if_iterate_finished
 
         small_letter:
             cmp bl, 7Ah
             ja symbol_count
-            inc lcase_letter
+            inc lower_case
             inc symbol_number ; is small letter a symbol?
             cmp bh, 20h
             jbe word_count
             cmp cx, 01
             je check_if_last_word
-
-            jmp subber
+            jmp increase_counter
         
         big_letter:   
             cmp bl, 5Ah
             ja symbol_count
-            inc dcase_letter
+            inc upper_case
             inc symbol_number
             cmp bh, 20h
             jbe word_count
             cmp cx, 01
             je check_if_last_word
-            
-            jmp subber
+            jmp increase_counter
 
         symbol_count:
             cmp bl, 7Fh
@@ -219,7 +164,7 @@ continue_checking:
             jb symbol_add
             cmp bl, 20h
             ja symbol_add
-            jmp subber
+            jmp increase_counter
         symbol_add:
             inc symbol_number
             cmp bh, 20h
@@ -228,12 +173,12 @@ continue_checking:
             cmp cx, 01h
             je check_if_last_word
 
-            jmp subber
+            jmp increase_counter
 
         check_if_last_word:
             cmp al, 10
             jne word_count
-            jmp subber
+            jmp increase_counter
 
 close_file:
             xor cx, cx
@@ -249,13 +194,13 @@ close_file:
     mov ah, 9   
 	mov dx, offset lcase
 	int 21h
-    mov ax, lcase_letter
+    mov ax, lower_case
     call HEX_TO_DEC
 
      mov ah, 9   
 	mov dx, offset dcase
 	int 21h
-    mov ax, dcase_letter
+    mov ax, upper_case
     call HEX_TO_DEC
 
      mov ah, 9   
@@ -263,21 +208,8 @@ close_file:
 	int 21h
     mov ax, symbol_number
     call HEX_TO_DEC
-    
-
-
-
-    
-
-
-
-
 
     jmp exit
-
-
-
-
 
 exit:
     mov ah, 4ch
@@ -314,155 +246,3 @@ RET
 endp HEX_TO_DEC
 
 end start
-
-Proc HEX_TO_DEC
-
-    xor dx, dx
-    xor cx, cx
-    xor bx, bx
-    mov cx, 10
-
-    divide:
-    div cx
-    push dx
-    xor dx, dx
-    inc bx
-    test ah, ah
-    jz check_al
-    check_al:
-    test al, al
-    jz print_number
-    jmp divide
-    print_number:
-    pop dx
-    add dl, 30h
-    mov ah, 02h
-    int 21h
-    dec bx
-    jnz print_number
-
-RET
-endp HEX_TO_DEC
-
-STUFF:  HEX_TO_DEC:
-    xor dx, dx
-    xor cx, cx
-    xor bx, bx
-    mov ax, word_number
-    mov cx, 10
-
-    divide:
-    div cx
-    push dx
-    xor dx, dx
-    inc bx
-    test ah, ah
-    jz check_al
-    check_al:
-    test al, al
-    jz print_number
-    jmp divide
-    print_number:
-    pop dx
-    add dl, 30h
-    mov ah, 02h
-    int 21h
-    dec bx
-    jnz print_number
-    jmp exit
-
-    ; PROC Count_symbols
-    ;     push ax
-    ;     push bx
-    ;     push cx
-    ;     push dx
-
-
-            
-            
-                ; ignore multiple spaces
-        
-
-        
-    
-    ;  MOV     CX,0FFFFh        ;the number to turn to Decimal
-    XOR     AX,0
-    xor    BX,0
-    xor     DL,0
-    INC_LOOP:
-    INC     AL               ;AL is the first digit
-    CMP     AL,0Ah           ;Has it reached 10 yet?
-    JNE     CONTINUE         ;No, continue.
-    MOV     AL,0             ;Yes, empty it
-    INC     AH      
-    push ax         ;AH is the second digit
-    CMP     AH,0Ah           ;Has it reached 10 yet?
-    JNE     CONTINUE         ;No, continue ...
-    MOV     AH,0
-    INC     BL               ;BL is the third digit
-    CMP     BL,0Ah
-    JNE     CONTINUE
-    MOV     BL,0
-    INC     BH               ;BH is the forth digit
-    CMP     BH,0Ah
-    MOV     BH,0
-    push bx
-    INC     DL               ;DL is the fifth digit
-            
-    CONTINUE:
-    pop ax
-    LOOP    INC_LOOP
-
-    ; HEX_TO_DEC:
-    ;                 XOR BX, BX
-    ;                 XOR AX, AX
-    ;                 MOV     CL, 10
-    ;                 MOV AL, dcase_letter
-                
-    ;                 LOOP1:
-    ;                 DIV CL
-    ;                 inc BX
-    ;                 PUSH AX
-    ;                 XOR AH, AH
-
-    ;                 TEST al, al
-    ;                 jnz LOOP1
-
-    ;                 LOOP2:
-    ;                 POP DX
-    ;                 MOV DL, DH
-    ;                 ADD DL, '0'
-    ;                 MOV AH, 02h
-    ;                 INT 21h
-    ;                 dec bx
-    ;                 jnz LOOP2
-        
-
-        ; mov ah, 9   
-        ; mov dx, offset dcase
-        ; int 21h
-
-        ;  HEX_TO_DEC:
-        ;             XOR BX, BX
-        ;             XOR AX, AX
-        ;             MOV bl, 10
-        ;             MOV ax, dcase_letter
-        ;             LOOP1:
-                
-
-        ;             DIV bl
-        ;             inc cx
-        ;             PUSH AX
-        ;             XOR AH, AH
-
-        ;             TEST al, al
-        ;             jnz LOOP1
-
-        ;             LOOP2:
-        ;             POP DX
-        ;             MOV DL, DH
-        ;             ADD DL, '0'
-        ;             MOV AH, 02h
-        ;             INT 21h
-        ;             dec cx
-        ;             jnz LOOP2
